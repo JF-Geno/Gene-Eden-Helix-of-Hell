ba9175e62068937dd9ea763cc2bcc0aecef4998c
JF-Geno <obondejfgeno@gmail.com>
2024-06-14 01:42:52 -0500

    the pain of animation (5 hours)

--- Assets/GameSystem/CreaturesController/PLayersController/Player.cs
+++ Assets/GameSystem/CreaturesController/PLayersController/Player.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Player : Creatures
 {  
-    public CharacterController Controller;
+  public CharacterController controller;
     public float speed = 12f;
+    public float runMultiplier = 2f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
@@ -13,50 +14,65 @@ public class Player : Creatures
     public float groundCheckDistance = 0.4f;
     public LayerMask groundMask;
 
-    Vector3 velocity;
-    bool isGrounded;
+    private Vector3 velocity;
+    private bool isGrounded;
+    
+    public Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
-        if (Controller == null)
-        {
-            Controller = GetComponent<CharacterController>();
-        }
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        
         isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
 
-        
+      
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f; 
+            velocity.y = -2f;
         }
 
-       
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-      
         Vector3 move = transform.right * x + transform.forward * z;
 
-        Controller.Move(move * speed * Time.deltaTime);
+        
+        float currentSpeed = speed;
+        if ( Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed *= runMultiplier;
+        }
 
-      
+        
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+       
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-        
+       
         velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
        
-        Controller.Move(velocity * Time.deltaTime);
+        bool isWalking = x != 0 || z != 0;
+        bool isRunning = isWalking && Input.GetKey(KeyCode.LeftShift);
+
+        animator.SetBool("isWalking", isWalking);
+        animator.SetBool("isRunning", isRunning);
     }
 }
